2e2bc3c815d93090e9e7fef61432f68e9c1f56f2
Daenamu <dheodnr318@naver.com>
2020-10-05 21:39:17 +0900

    blog/bookmark app user CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -39,4 +39,16 @@ urlpatterns = [
 
     # Example: /blog/search/
     path('search/', views.SearchFormView.as_view(), name='search'),
+
+    # Example: /blog/add/
+    path('add/', views.PostCreateView.as_view(), name='add'),
+
+    # Example:  /blog/change/
+    path('change/', views.PostChangeLV.as_view(), name="change"),
+
+    # Example:  /blog/change/
+    path('<int:pk>/update/', views.PostUpdateView.as_view(), name="update"),
+
+    # Example:  /blog/change/
+    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name="delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, TemplateView, FormView
+from django.views.generic import ListView, DetailView, TemplateView, FormView, CreateView, DeleteView, UpdateView
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, TodayArchiveView
 from blog.models import Post
 from blog.forms import PostSearchForm
 from django.conf import settings
 from django.db.models import Q
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from mysite.views import OwnerOnlyMixin
 
 # Create your views here.
 
@@ -88,3 +90,32 @@ class SearchFormView(FormView):
         context['object_list'] = post_list
 
         return render(self.request, self.template_name, context)
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'slug', 'description', 'content', 'tags']
+    initial = {'slug': 'auto-filling-do-not-input'}
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class PostChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'blog/post_change_list.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(owner=self.request.user)
+
+
+class PostUpdateView(OwnerOnlyMixin, UpdateView):
+    model = Post
+    fields = ['title', 'slug', 'description', 'content', 'tags']
+    success_url = reverse_lazy('blog:index')
+
+
+class PostDeleteView(OwnerOnlyMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:index')
